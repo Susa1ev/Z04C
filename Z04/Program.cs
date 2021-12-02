15611fb7cb3fb64fc2df0d7b2ac7b5331b29f685
Susa1ev <susalytt@gmail.com>
2021-12-02 16:00:53 +0300

    Добавьте файлы проекта.

---
+++ Z04/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Z041
+{
+    class prac
+    {
+        int a, b;
+        public prac()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите два целочисленных значения:");
+                if (Int32.TryParse(Console.ReadLine(), out a) &&
+                    Int32.TryParse(Console.ReadLine(), out b))
+                {
+                    Console.WriteLine(nod(Math.Abs(a), Math.Abs(b)));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели что-то не то, попробуйте ещё раз");
+                }
+
+            }
+        }
+        int nod(int a, int b)
+        {
+            if (a == b) { return a; }
+            else if (a > b)
+            {
+                return nod(a - b, b);
+            }
+            else
+            {
+                return nod(a, b - a);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+}
---
+++ Z042/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Z042
+{
+    class prac
+    {
+        public prac()
+        {
+            while(true)
+            {
+                Console.WriteLine("Введите n:");
+                if (Int32.TryParse(Console.ReadLine(), out k))
+                {
+                    rec(k, i);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Вы ввели что-то не то, попробуйте ещё раз");
+                }
+
+            }
+        }
+        int i = 0, k;
+        void str(int i, string c)
+        {
+            int j = 0;
+            while (j < i)
+            {
+                j++;
+                Console.Write(c);
+            }
+        }
+        void rec(int n, int i)
+        {
+            if (n >= 0)
+            {
+                str(i, "*");
+                str(n, " ");
+                str(i, "*");
+                Console.WriteLine();
+                rec(n - 2, i + 1);
+                str(i, "*");
+                str(n, " ");
+                str(i, "*");
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            prac p = new prac();
+        }
+    }
+
+}
